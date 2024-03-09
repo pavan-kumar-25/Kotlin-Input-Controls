714d6cd5d6e74f89ac01b0829dba905f8649c2a1
pavan-kumar-25 <pavankumar.c@mca.christuniversity.in>
2024-03-09 19:42:01 +0530

    new

--- app/src/main/java/com/example/input_controls/MainActivity.kt
+++ app/src/main/java/com/example/input_controls/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.input_controls
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.MediaController
@@ -16,11 +16,14 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private val REQUEST_CODE_GALLERY = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val videoView = findViewById<VideoView>(R.id.videoView)
+        val btnAddImage = findViewById<Button>(R.id.btn_image)
+        imageView = findViewById(R.id.imageView)
 
         val mediaController = MediaController(this)
 
@@ -32,11 +35,9 @@ class MainActivity : AppCompatActivity() {
         videoView.start()
 
         val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.loadUrl("https://www.indiatoday.in/blogs-section")
-
-
-        val btnAddImage: Button = findViewById(R.id.btn_image)
-        imageView = findViewById(R.id.imageView)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+        myWebView.loadUrl("https://christuniversity.in/")
 
         btnAddImage.setOnClickListener {
             openGallery()
@@ -59,7 +60,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
-
-
